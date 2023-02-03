22be8fa95cd572b83dcb58f97c4cdba49ced8d2c
kjhtjkyj6 <singhkuljinder497@gmail.com>
2023-02-02 20:16:12 -0500

    Updating Assignment 02

--- FavouriteAthletes/src/app/content-list/content-list.component.ts
+++ FavouriteAthletes/src/app/content-list/content-list.component.ts
@@ -8,41 +8,49 @@ import { Content } from '../helper-files/content-interface';
 export class ContentListComponent implements OnInit {
 
   contentItem:Content[] = [{
-    id:1,
+    id:0,
     title:"Javelin",
     description:"Javelin is one of the favourite games",
     creator:"Kuljinder Singh",
     type:"Athetics",
     imgURL:"https://3.bp.blogspot.com/-W5Am6DMqhJo/Vum2x4AeFvI/AAAAAAAAiIU/vtXAw11pHuA6dRun-SzW9Fc_ldlLkEfAg/s1600/Javelin%2BThrow%2BSport%2B02.png"  },
  {
-    id:2,
+    id:1,
     title:"Track Races",
     description:"Track Race is one of the favourite games",
     creator:"Kuljinder Singh",
     type:"Athletics",
     imgURL:"https://th.bing.com/th/id/R.b2b264385f8cfc10c8b5cfd91ed46026?rik=Yful9fnwpLdriw&riu=http%3a%2f%2fcdn.fansided.com%2fwp-content%2fblogs.dir%2f229%2ffiles%2f2016%2f08%2f9463583-justin-gatlin-yohan-blake-olympics-track-and-field-850x520.jpg&ehk=xXMw23XhZg60fJmmQDq3Jdcok9QPEV9%2f7r5RZdigX9I%3d&risl=&pid=ImgRaw&r=0"  }
  , {
-    id:3,
+    id:2,
     title:"Jumps",
     description:"Jumps are one of the favourite games ",
     creator:"Kuljinder Singh",
     type:"Athletics",
     imgURL:"https://th.bing.com/th/id/R.3f6055aa4b71481878aae9cb20d3cdd8?rik=CQNeo90zOmXl1Q&riu=http%3a%2f%2fathletics.ca%2fwp-content%2fuploads%2f2015%2f01%2fDerek-Drouin-CAN-001-HJ_28_07_14CA-e1422911700750.jpg&ehk=T1gtHTj9dXRV2Te83%2bT9GulBDA5zRyLLyQxqi8PAGuo%3d&risl=&pid=ImgRaw&r=0"  }
   , {
-    id:4,
+    id:3,
     title:"Swimming",
     description:"Swimming is one of the favourite games ",
     creator:"Kuljinder Singh",
     type:"Athletics",
     imgURL:"https://jooinn.com/images/swimming-championship-3.jpg"  }
   ,{
-    id:5,
+    id:4,
     title:"Races",
     description:"Racing events are one of the favourite games ",
     creator:"Kuljinder Singh",
     type:"Athletics",
-    imgURL:"https://th.bing.com/th/id/R.275881e63d97053aa1d7b8a3b6329815?rik=eymTBmrD4FumNQ&riu=http%3a%2f%2fwww.abudhabi2.com%2fwp-content%2fuploads%2f2016%2f08%2fmaxresdefault1.jpg&ehk=Ow6wnnSIIafRj1TEfGZeXnPBC2nfxm%2bLeIR96I68wSc%3d&risl=&pid=ImgRaw&r=0"  }
-];
+    imgURL:"https://th.bing.com/th/id/R.275881e63d97053aa1d7b8a3b6329815?rik=eymTBmrD4FumNQ&riu=http%3a%2f%2fwww.abudhabi2.com%2fwp-content%2fuploads%2f2016%2f08%2fmaxresdefault1.jpg&ehk=Ow6wnnSIIafRj1TEfGZeXnPBC2nfxm%2bLeIR96I68wSc%3d&risl=&pid=ImgRaw&r=0"  },
+
+    {
+      id:5,
+      title:"Throwing Events",
+      description:"Throwing events are one of the favourite games ",
+      creator:"Kuljinder Singh",
+      type:"Athletics",
+      imgURL:"https://i1.wp.com/thesportslite.com/wp-content/uploads/2021/08/discus-throw-2.jpg?w=1247&ssl=1"  }
+  ];
 
 
 constructor() { 
