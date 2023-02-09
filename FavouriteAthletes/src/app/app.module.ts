7244ead75f570faa83e5fd72f40ee3adf3505912
kjhtjkyj6 <singhkuljinder497@gmail.com>
2023-02-09 18:44:46 -0500

    Assignment 03

--- FavouriteAthletes/src/app/app.module.ts
+++ FavouriteAthletes/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { leadingComment } from '@angular/compiler';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { PipePipe } from './pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    ContentListComponent,
+
+    PipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
