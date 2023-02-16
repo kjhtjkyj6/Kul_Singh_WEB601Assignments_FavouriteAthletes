27ee0f4afc28ce3a9f9e50d83aa3a7860a48da83
kjhtjkyj6 <singhkuljinder497@gmail.com>
2023-02-15 21:48:07 -0500

    Updating Assignment 04

--- FavouriteAthletes/src/app/content-card/content-card.component.ts
+++ FavouriteAthletes/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
-import { Component OnInit} from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, Input, OnInit } from '@angular/core';
+
 import { Content } from '../helper-files/content-interface';
 
 
@@ -10,35 +10,12 @@ import { Content } from '../helper-files/content-interface';
 })
 export class ContentCardComponent  {
 
-  contentItem:Content = {
-    id:1,
-    title:"Javelin",
-    description:"Javelin is one of the favourite games",
-    creator:"Kuljinder Singh",
-    type:"Athetics"
-  }
-  contentItem2:Content = {
-    id:2,
-    title:"Track Races",
-    description:"Track Race is one of the favourite games",
-    creator:"Kuljinder Singh",
-    type:"Athletics"
-  }
-  contentItem3:Content = {
-    id:3,
-    title:"Jumps",
-    description:"Jumps are one of the favourite games ",
-    creator:"Kuljinder Singh",
-    type:"Athletics"
-  }
-  myContent = new ContentList(this.contentItem)
-  contentList = this.myContent.getContent();
-  
-  constructor() {
-    this.myContent.add(this.contentItem);
-    this.myContent.add(this.contentItem2);
-    this.myContent.add(this.contentItem3);
-   
+  default:string="/assets/imgs/photo.jpg"
 
+ @Input() items:any ;
+public Onclick(content:any)
+{
+  console.log("Id="+content.id +"  Title="+content.title );
 }
+
 }
